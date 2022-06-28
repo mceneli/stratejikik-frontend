d9ed29ec9c9b1f21089ddcf1bc7178327537cbab
mceneli <muhammetceneli@gmail.com>
2022-06-29 00:00:46 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component }  from 'react';
+import Navbar from "./Navbar"
+import Pricing from "./pages/Pricing"
+import Register from "./pages/Register"
+import About from "./pages/About"
+import { Route, Routes } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Pricing.js
@@ -0,0 +1,5 @@
+import React, { Component }  from 'react';
+
+export default function Pricing() {
+  return <h1>Pricing</h1>
+}
